7c1ea1a48697c4d837de65e8613a1358077ba572
Jay Taylor <outtatime@gmail.com>
2018-03-17 20:25:37 -0700

    sync with upstream: First pass at extraction of article date published field

--- configuration.go
+++ configuration.go
@@ -1,7 +1,6 @@
 package goose
 
 import (
-	"github.com/gravicle/gojs-config"
 	"time"
 )
 
@@ -44,7 +43,7 @@ func GetDefaultConfiguration(args ...string) Configuration {
 			imageMagickIdentifyPath: "/usr/bin/identify", //not used in this version
 			browserUserAgent:        defaultUserAgent,
 			debug:                   false,
-			extractPublishDate:      false,
+			extractPublishDate:      true,
 			additionalDataExtractor: false,
 			stopWordsPath:           "resources/stopwords",
 			stopWords:               NewStopwords(), //TODO with path
@@ -54,27 +53,20 @@ func GetDefaultConfiguration(args ...string) Configuration {
 		}
 	}
 
-	path := args[0]
-	jsconfiguration, err := jsconfig.LoadConfig(path)
-	if err != nil {
-		panic(err.Error())
-	}
-	stopWordsPath := jsconfiguration.String("stopWordsPath", "resources/stopwords")
-	stopWords := NewStopwords() //TODO with path
 	return Configuration{
-		localStoragePath:        jsconfiguration.String("localStoragePath", ""), //not used in this version
-		imagesMinBytes:          jsconfiguration.Int("imageMinBytes", 4500),     //not used in this version
-		enableImageFetching:     jsconfiguration.Bool("enableImageFetching", true),
-		useMetaLanguage:         jsconfiguration.Bool("useMetaLanguage", true),
-		targetLanguage:          jsconfiguration.String("targetLanguage", "en"),
-		imageMagickConvertPath:  jsconfiguration.String("imageMagickConvertPath", "/usr/bin/convert"),   //not used in this version
-		imageMagickIdentifyPath: jsconfiguration.String("imageMagickIdentityPath", "/usr/bin/identify"), //not used in this version
-		browserUserAgent:        jsconfiguration.String("browserUserAgent", defaultUserAgent),
-		debug:                   jsconfiguration.Bool("debug", false),
-		extractPublishDate:      jsconfiguration.Bool("extractPublishDate", false),      //TODO
-		additionalDataExtractor: jsconfiguration.Bool("additionalDataExtractor", false), //TODO
-		stopWordsPath:           stopWordsPath,
-		stopWords:               stopWords,
+		localStoragePath:        "",   //not used in this version
+		imagesMinBytes:          4500, //not used in this version
+		enableImageFetching:     true,
+		useMetaLanguage:         true,
+		targetLanguage:          "en",
+		imageMagickConvertPath:  "/usr/bin/convert",  //not used in this version
+		imageMagickIdentifyPath: "/usr/bin/identify", //not used in this version
+		browserUserAgent:        defaultUserAgent,
+		debug:                   false,
+		extractPublishDate:      true,
+		additionalDataExtractor: false,
+		stopWordsPath:           "resources/stopwords",
+		stopWords:               NewStopwords(), //TODO with path
 		parser:                  NewParser(),
 		timeout:                 time.Duration(jsconfiguration.Int("timeout", 5)) * time.Second,
 		//titleDelimiters:         jsconfiguration.Array("titleDelimiters"),
